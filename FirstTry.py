e2ee1cbbf06c2ac352562e1f965c70dc14896c59
Saneladam <romangarciaguill@gmail.com>
2026-03-18 17:02:47 +0100

    Wed Mar 18 05:02:47 PM CET 2026

--- FirstTry.py
+++ FirstTry.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 # =============================================================================
-# Authors:      Román García Guill
+# Authors:      Francium Gray
+#               Román García Guill
+#
 # Contact:      romangarciaguill@gmail.com
 # Created:      Wed 18. Mar 2026
 #
@@ -13,10 +15,8 @@ from pathlib import Path
 import numpy as np
 
 
-
 def main() -> None:
     pass
 
 if __name__ == "__main__":
     main()
-

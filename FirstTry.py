6af49b8810df519edda894916b11d58c13592537
Saneladam <romangarciaguill@gmail.com>
2026-03-18 16:30:42 +0100

    Wed Mar 18 04:30:42 PM CET 2026

---
+++ FirstTry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# =============================================================================
+# Authors:      Román García Guill
+# Contact:      romangarciaguill@gmail.com
+# Created:      Wed 18. Mar 2026
+#
+# Purpose:      Get an Image and create an Ascii from it
+# =============================================================================
+
+from pathlib import Path
+
+import numpy as np
+
+
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
+
